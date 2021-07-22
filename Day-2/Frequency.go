d9d0b318404ae47f38aabc169ec87ec2d7999c0c
Himanshu Jha <35391094+HimanshuJha1702@users.noreply.github.com>
2021-07-22 16:42:10 +0530

    update mutex

--- Day-2/Frequency.go
+++ Day-2/Frequency.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"sync"
 )
 
-func frequency (wg *sync.WaitGroup, s string, mmap map[string]int)  {
-	var mutex = &sync.Mutex{}
+func frequency (wg *sync.WaitGroup, s string, mmap map[string]int, m *sync.Mutex)  {
+
 	defer wg.Done()
 	for i:=0; i<len(s); i++ {
 		key := string(s[i])
-		mutex.Lock()
+		m.Lock()
 		mmap[key]++
-		mutex.Unlock()
+		m.Unlock()
 	}
 }
 
@@ -32,17 +33,23 @@ func main() {
 
 	wg := new(sync.WaitGroup)
 	wg.Add(n)
-
+	var m sync.Mutex
 	for i:=0;i<n;i++  {
 		var temp string
 		temp = allWords[i]
-		go frequency(wg, temp, mmap)
+		go frequency(wg, temp, mmap, &m)
 	}
 
 	wg.Wait()
 
-	for k, v := range mmap {
-		fmt.Println(k, "value is", v)
+	keys := make([]string, 0, len(mmap))
+	for k := range mmap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		fmt.Println(k, "value is", mmap[k])
 	}
 
 }
